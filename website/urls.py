994886c2e9d3cd18d99c9ac64094a142c5880d99
marine.richer <marine.richer@prune.sh>
2026-01-23 21:37:26 +0100

    danser

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from website.views import (
-    home_view,
-)
+from website.views import dance_view, home_view
 
 app_name = "website"
 
 urlpatterns = [
     path("", home_view, name="home"),
+    path("danser/", dance_view, name="dance"),
 ]
--- website/views.py
+++ website/views.py
@@ -5,3 +5,8 @@ from django.views.decorators.http import require_GET
 @require_GET
 def home_view(request):
     return render(request, "website/pages/page.html")
+
+
+@require_GET
+def dance_view(request):
+    return render(request, "website/pages/danser/page.html")
