7e9d738b5f64c78dc0e30032f78ec9c12200de8a
marine.richer <marine.richer@prune.sh>
2026-01-24 01:12:01 +0100

    danser, projet et contact

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from website.views import dance_view, home_view
+from website.views import contact_view, dance_view, home_view, project_view
 
 app_name = "website"
 
 urlpatterns = [
     path("", home_view, name="home"),
     path("danser/", dance_view, name="dance"),
+    path("projets/", project_view, name="project"),
+    path("contact/", contact_view, name="contact"),
 ]
--- website/views.py
+++ website/views.py
@@ -10,3 +10,13 @@ def home_view(request):
 @require_GET
 def dance_view(request):
     return render(request, "website/pages/danser/page.html")
+
+
+@require_GET
+def project_view(request):
+    return render(request, "website/pages/projets/page.html")
+
+
+@require_GET
+def contact_view(request):
+    return render(request, "website/pages/contact/page.html")
