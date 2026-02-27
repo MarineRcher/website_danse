87dbb5e5f8cdfce9a662db8222eca62e9004473a
marine.richer <marine.richer@prune.sh>
2026-02-27 17:58:08 +0100

    robots.txt

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,17 @@
+import os
+
+from django.conf import settings
+from django.http import HttpResponse
 from django.urls import path
 
 from website.views import contact_view, dance_view, home_view, project_view
+
+
+def robots_txt(request):
+    path = os.path.join(settings.BASE_DIR, "website/static/website/robots.txt")
+    with open(path) as f:
+        return HttpResponse(f.read(), content_type="text/plain")
+
 
 app_name = "website"
 
@@ -9,4 +20,5 @@ urlpatterns = [
     path("danser/", dance_view, name="dance"),
     path("projets/", project_view, name="project"),
     path("contact/", contact_view, name="contact"),
+    path("robots.txt", robots_txt),
 ]
