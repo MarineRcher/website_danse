ead8006365daf65b2cadfe4948e4c3ee820cd08a
marine.richer <marine.richer@prune.sh>
2026-03-19 12:11:18 +0100

    redirect www

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
     priority = 0.8
+    changefreq = "monthly"
 
     def items(self):
         return ["website:home", "website:dance", "website:project", "website:contact"]
